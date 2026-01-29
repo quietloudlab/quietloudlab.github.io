76719b138309505c552e13c76bf2ee686741fd32
Brandon Harwood <brandon@quietloudlab.com>
2026-01-29 14:34:04 -0600

    Update to new Formspark form IDs

--- src/main.tsx
+++ src/main.tsx
@@ -173,7 +173,7 @@ const ContactForm = ({ contactIntent }: { contactIntent: ContactIntent | null })
     const formData = new FormData(e.currentTarget);
 
     try {
-      const response = await fetch("https://submit-form.com/gpAWtEfDu", {
+      const response = await fetch("https://submit-form.com/zJR7lYKAh", {
         method: "POST",
         body: formData,
         headers: {
@@ -304,7 +304,7 @@ const NewsletterForm = () => {
     const formData = new FormData(e.currentTarget);
 
     try {
-      const response = await fetch("https://submit-form.com/rFddulOOu", {
+      const response = await fetch("https://submit-form.com/buMSWec82", {
         method: "POST",
         body: formData,
         headers: {
